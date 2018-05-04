079c8182496000cdd60877b2d7c77e6c02089879
noelfernandes <webnoel@yahoo.com>
2018-05-04 14:53:35 +0400

    Add files via upload

---
+++ SOKTrials/SOKTrials/SOKTrials/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using SlideOverKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SOKTrials
+{
+	public partial class MainPage : ContentPage, IMenuContainerPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            this.ToolbarItems.Add(new ToolbarItem
+            {
+                Command = new Command(() =>
+                {
+                    if (this.SlideMenu.IsShown)
+                    {
+                        HideMenuAction?.Invoke();
+                    }
+                    else
+                    {
+                        ShowMenuAction?.Invoke();
+                    }
+                }),
+                Icon = "icon.png",
+                Text = "Settings",
+                Priority = 0
+            });
+            this.SlideMenu = new SettingsPage();
+		}
+
+        public SlideMenuView SlideMenu { get; set; }
+        public Action ShowMenuAction { get; set; }
+        public Action HideMenuAction { get; set; }
+    }
+}
---
+++ SOKTrials/SOKTrials/SOKTrials/SettingsPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using SlideOverKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SOKTrials
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsPage : SlideMenuView
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+
+            #region SlideDownMenu
+            // You must set HeightRequest in this case
+            this.HeightRequest = 600;
+            // You must set IsFullScreen in this case, 
+            // otherwise you need to set WidthRequest, 
+            // just like the QuickInnerMenu sample
+            this.IsFullScreen = true;
+            this.MenuOrientations = MenuOrientation.TopToBottom;
+
+            // You must set BackgroundColor, 
+            // and you cannot put another layout with background color cover the whole View
+            // otherwise, it cannot be dragged on Android
+            this.BackgroundColor = Color.FromHex("#D8DDE7");
+            #endregion
+
+            #region RightSideMasterView
+            //// You must set IsFullScreen in this case, 
+            //// otherwise you need to set HeightRequest, 
+            //// just like the QuickInnerMenu sample
+            //this.IsFullScreen = true;
+            //// You must set WidthRequest in this case
+            //this.WidthRequest = 250;
+            //this.MenuOrientations = MenuOrientation.RightToLeft;
+
+            //// You must set BackgroundColor, 
+            //// and you cannot put another layout with background color cover the whole View
+            //// otherwise, it cannot be dragged on Android
+            //this.BackgroundColor = Color.White;
+
+            //// This is shadow view color, you can set a transparent color
+            //this.BackgroundViewColor = Color.FromHex("#CE766C");
+            #endregion
+
+        }
+	}
+}
